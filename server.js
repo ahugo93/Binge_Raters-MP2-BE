6745256fe6d2f04356166c9880ffa60d2d4f9ab4
Anibal Hugo <ani2bal@yahoo.com>
2022-10-17 18:50:22 -0400

    fixed first bug

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to Binge Raters!')
@@ -13,7 +19,7 @@ app.get('/', (req, res) => {
 
 // SHOWS
 const showsController = require('./controllers/shows_controller.js')
-app.use('/shows, showsController')
+app.use('/shows', showsController)
 
 // LISTEN
 app.listen(PORT, () => {
